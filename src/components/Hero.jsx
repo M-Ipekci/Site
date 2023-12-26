109405264d989d9edc9f3477504324dda23a7560
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-25 21:44:06 -0800

    2nd load

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { content } from "../Content";
 
 const Hero = () => {
-  const { hero } = content;
   const [showContent, setShowContent] = useState(false);
 
   useEffect(() => {
+    // Trigger the effect directly without waiting for scroll
     setShowContent(true);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
   }, []);
-
-  const handleScroll = () => {
-  };
 
   return (
     <section id="home" className="relative overflow-hidden">
       <div className="min-h-screen relative flex md:flex-row flex-col-reverse md:items-end justify-center items-center">
         <div
           className={`absolute h-full md:w-4/12 w-8/12 top-0 right-0 bottom-0 z-10 ${
-            showContent ? "opacity-100 translate-x-0" : "opacity-0 translate-x-10"
+            showContent ? "opacity-100 translate-x-0 transition-opacity ease-in duration-500" : "opacity-0 translate-x-10"
           } bg-primaryLinear`}
         >
-          <h2 className="rotate-90 absolute top-[30%] right-[15%]" style={{ fontSize: '3rem' }}>{hero.title}</h2>
+          <h2 className="rotate-90 absolute top-[30%] right-[15%]" style={{ fontSize: '3rem' }}>Your Hero Title</h2>
         </div>
 
         <div className={`pb-16 px-6 pt-12 z-10 text-center-mobile ${showContent ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}>
@@ -54,6 +47,7 @@ const Hero = () => {
               zIndex: 1000,
             }}
           >
+            {/* Your Spline 3D model component was here */}
           </div>
         )}
       </div>
