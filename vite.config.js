1b832af9422e45ddba9c2abfbb1ba69515a3cd70
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-26 16:24:24 -0800

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default {
   plugins: [react()],
-});
-
+  build: {
+    rollupOptions: {
+      external: [
+        'react',
+        'react-dom',
+        'react-three-fiber', // Add react-three-fiber to the external dependencies
+      ],
+    },
+  },
+};
